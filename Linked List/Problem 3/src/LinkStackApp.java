3b32adebc4e912b609244a785d75ee7a0e852e6b
MrT2008 <haitrieu6212@gmail.com>
2024-04-13 22:28:29 +0700

    submit

--- Linked List/Problem 3/src/LinkStackApp.java
+++ Linked List/Problem 3/src/LinkStackApp.java
@@ -1,104 +1,157 @@
-class Link
+class Link {
+   public long dData; // data item
+   public Link next; // next link in list
+   // -------------------------------------------------------------
+
+   public Link(long dd) // constructor
    {
-   public long dData;             // data item
-   public Link next;              // next link in list
-// -------------------------------------------------------------
-   public Link(long dd)           // constructor
-      { dData = dd; }
-// -------------------------------------------------------------
-   public void displayLink()      // display ourself
-      { System.out.print(dData + " "); }
-   }  // end class Link
+      dData = dd;
+   }
+
+   // -------------------------------------------------------------
+   public void displayLink() // display ourself
+   {
+      System.out.print(dData + " ");
+   }
+
+   public long getdData() {
+      return dData;
+   }
+} // end class Link
 ////////////////////////////////////////////////////////////////
-class LinkList
+
+class LinkList {
+   private Link first; // ref to first item on list
+   // -------------------------------------------------------------
+
+   public LinkList() // constructor
    {
-   private Link first;            // ref to first item on list
-// -------------------------------------------------------------
-   public LinkList()              // constructor
-      { first = null; }           // no items on list yet
-// -------------------------------------------------------------
-   public boolean isEmpty()       // true if list is empty
-      { return (first==null); }
-// -------------------------------------------------------------
+      first = null;
+   } // no items on list yet
+   // -------------------------------------------------------------
+
+   public boolean isEmpty() // true if list is empty
+   {
+      return (first == null);
+   }
+
+   // -------------------------------------------------------------
    public void insertFirst(long dd) // insert at start of list
-      {                           // make new link
+   { // make new link
       Link newLink = new Link(dd);
-      newLink.next = first;       // newLink --> old first
-      first = newLink;            // first --> newLink
-      }
-// -------------------------------------------------------------
-   public long deleteFirst()      // delete first item
-      {                           // (assumes list not empty)
-      Link temp = first;          // save reference to link
-      first = first.next;         // delete it: first-->old next
-      return temp.dData;          // return deleted link
-      }
-// -------------------------------------------------------------
-   public void displayList()
+      newLink.next = first; // newLink --> old first
+      first = newLink; // first --> newLink
+   }
+
+   // -------------------------------------------------------------
+   public long deleteFirst() // delete first item
+   { // (assumes list not empty)
+      Link temp = first; // save reference to link
+      first = first.next; // delete it: first-->old next
+      return temp.dData; // return deleted link
+   }
+
+   // -------------------------------------------------------------
+   public void displayList() {
+      Link current = first; // start at beginning of list
+      while (current != null) // until end of list,
       {
-      Link current = first;       // start at beginning of list
-      while(current != null)      // until end of list,
-         {
-         current.displayLink();   // print data
-         current = current.next;  // move to next link
-         }
+         current.displayLink(); // print data
+         current = current.next; // move to next link
+      }
       System.out.println("");
+   }
+
+   public int getLong(){
+      int counter=0;
+
+      Link current = first; 
+      while(current != null)      // until end of list,
+      {
+      current = current.next; 
+      counter++; 
       }
-// -------------------------------------------------------------
-   }  // end class LinkList
+
+      return counter; 
+
+    }
+
+   public long getFirst() {
+      return first.getdData();
+   }
+   // -------------------------------------------------------------
+} // end class LinkList
 ////////////////////////////////////////////////////////////////
-class LinkStack
-   {
+
+class LinkStack {
    private LinkList theList;
-//--------------------------------------------------------------
-   public LinkStack()             // constructor
-      {
+
+   // --------------------------------------------------------------
+   public LinkStack() // constructor
+   {
       theList = new LinkList();
-      }
-//--------------------------------------------------------------
-   public void push(long j)     // put item on top of stack
-      {
+   }
+
+   // --------------------------------------------------------------
+   public void push(long j) // put item on top of stack
+   {
       theList.insertFirst(j);
-      }
-//--------------------------------------------------------------
-   public long pop()            // take item from top of stack
-      {
+   }
+
+   // --------------------------------------------------------------
+   public long pop() // take item from top of stack
+   {
       return theList.deleteFirst();
-      }
-//--------------------------------------------------------------
-   public boolean isEmpty()       // true if stack is empty
-      {
-      return ( theList.isEmpty() );
-      }
-//--------------------------------------------------------------
-   public void displayStack()
-      {
+   }
+
+   // --------------------------------------------------------------
+   public boolean isEmpty() // true if stack is empty
+   {
+      return (theList.isEmpty());
+   }
+
+   // --------------------------------------------------------------
+   public void displayStack() {
       System.out.print("Stack (top-->bottom): ");
       theList.displayList();
+   }
+
+   public void reverseList() // insert at start of list
+   { // make new link
+      LinkList newLink = new LinkList();
+      // System.out.println(theList.getLong());
+      for (int i = theList.getLong(); i != 0; i--) {
+         newLink.insertFirst(theList.getFirst());
+         theList.deleteFirst();
+
       }
-//--------------------------------------------------------------
-   }  // end class LinkStack
+      System.out.println("--------------------------------\nThe reversed list: ");
+      newLink.displayList();
+
+   }
+   // --------------------------------------------------------------
+} // end class LinkStack
 ////////////////////////////////////////////////////////////////
-class LinkStackApp
-   {
-   public static void main(String[] args)
-      {
+
+class LinkStackApp {
+   public static void main(String[] args) {
       LinkStack theStack = new LinkStack(); // make stack
 
-      theStack.push(20);                    // push items
+      theStack.push(20); // push items
       theStack.push(40);
 
-      theStack.displayStack();              // display stack
+      // theStack.displayStack(); // display stack
 
-      theStack.push(60);                    // push items
+      theStack.push(60); // push items
       theStack.push(80);
 
-      theStack.displayStack();              // display stack
+      theStack.displayStack(); // display stack
 
-      theStack.pop();                       // pop items
-      theStack.pop();
+      // theStack.pop(); // pop items
+      // theStack.pop();
 
-      theStack.displayStack();              // display stack
-      }  // end main()
-   }  // end class LinkStackApp
+      // theStack.displayStack();              // display stack
+      theStack.reverseList();
+   } // end main()
+} // end class LinkStackApp
 ////////////////////////////////////////////////////////////////
--- Linked List/Problem 4/src/LinkQueueApp.java
+++ Linked List/Problem 4/src/LinkQueueApp.java
@@ -12,36 +12,27 @@ class LinkQueueApp {
       theQueue.insert(80);
       theQueue.displayQueue(); // display queue
 
-      // theQueue.remove(); // remove items
-      // theQueue.remove();
       removed = theQueue.delayedRemove(3);
       removed = theQueue.delayedRemove(3);
-      // removed = theQueue.delayedRemove(2);
       theQueue.displayQueue(); // display queue
 
       System.out.println("---------------------------------------");
-      int avgServiceTime = 5; // minutes (adjust as needed)
-      int serviceTimeVariation = 2; // minutes (adjust as needed)
+      int avgTime = 5;
+      int serviceTimeVn = 2;
 
-      // Create a queue for customers
       LinkQueue customerQueue = new LinkQueue();
 
-      // Simulation parameters (adjust as needed)
-      int simulationTime = 30; // minutes
-      int arrivalRate = 2; // customers per minute (adjust as needed)
+      int simulationTime = 30; 
+      int arrivalRate = 2; 
 
       Random random = new Random();
 
-      // Simulate customer arrivals and service
       for (int time = 0; time < simulationTime; time++) {
-         // Check for customer arrival
          if (random.nextInt(60 / arrivalRate) == 0) {
-            // Generate random service time with variation
-            int serviceTime = avgServiceTime + random.nextInt(2 * serviceTimeVariation + 1) - serviceTimeVariation;
+            int serviceTime = avgTime + random.nextInt(2 * serviceTimeVn + 1) - serviceTimeVn;
             customerQueue.insert(serviceTime);
          }
 
-         // Process customer at the front of the queue (if any)
          if (!customerQueue.isEmpty()) {
             int customerServiceTime = (int) customerQueue.delayedRemove(1);
             if (customerServiceTime != -1) {
